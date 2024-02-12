3fb47aaa07b0e41b764b8efdcd74dbcadf6c2692
webdevyoo <yjk7454@naver.com>
2024-02-13 00:13:09 +0900

    feat: CreatAcount 회원가입폼 value, onChange 기능 구현

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -37,17 +37,46 @@ const Error = styled.span`
   color: tomato;
 `
 export default function CreateAccount() {
+  const [isLoading, setLoading] = useState(false);
 
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {target: {name, value}} = e;
+    if(name === "name") {
+      setName(value);
+    } else if(name === "email") {
+      setEmail(value)
+    } else if(name === "password") {
+      setPassword(value)
+    }
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      // 계정 생성
+      // 사용자 프로필이름 지정
+      // 홈페이지 리다이렉트
+    } catch (e) {
+      // setError
+    } finally {
+      setLoading(false)
+    }
+    console.log(name, email, password)
+  }
 
   return (
   <Wrapper>
     <Title>Log into❌</Title>
-    <Form>
-      <Input name="name" placeholder="Name" type="text" required/>
-      <Input name="email" placeholder="Email" type="email" required/>
-      <Input name="password" placeholder="Password" type="password" required/>
-      <Input type="submit" value={"Create Account"}/>
+    <Form onSubmit={onSubmit}>
+      <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required/>
+      <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required/>
+      <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required/>
+      <Input onChange={onChange} type="submit" value={isLoading ? "Loading...." : "Create Account"}/>
     </Form>
     {error !== "" ? <Error>{error}</Error>: null}
   </Wrapper>
