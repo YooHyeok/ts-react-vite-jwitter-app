65c29c6ce6fa4902de333986809b8cf8b1d7bd18
YooHyeok <yjk7454@naver.com>
2024-02-16 00:39:59 +0900

    feat: 기존 Global Style 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
-
-const GlobalStyles = createGlobalStyle`
-  ${reset};
-  * {
-  box-sizing: border-box;
-  }
-  body {
-  background-color: black;
-  color: white;
-  font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-`
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <>
-    <GlobalStyles/>
     <App />
-  </>,
 )
