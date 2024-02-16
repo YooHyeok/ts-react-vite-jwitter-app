f1cde4551659bec36c34771da95f4d25f71a5c70
webdevyoo <yjk7454@naver.com>
2024-02-16 10:51:03 +0900

    styled: 트윗- 프로필 사진 위치를 우측으로 옮기기 위해 그리드 조정

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -3,7 +3,7 @@ import { ITweet } from "./timeline";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 3fr 1fr;
+  grid-template-columns: 5fr 1fr;
   padding: 20px;
   border: 1px solid rgba(255, 255, 255, 0.5);
   border-radius: 15px;
