94771bb388fc7bb9ea566d926767607e893300ea
webdevyoo <yjk7454@naver.com>
2024-02-17 02:48:07 +0900

    feat: 기본 아바타 적용 버튼 추가 - storage, photoURL 제거

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import { auth, storage } from "./firebase"
 import { useState } from "react"
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+import { deleteObject, getDownloadURL, ref, uploadBytes } from "firebase/storage"
 import { updateProfile } from "firebase/auth"
 
 const Wrapper = styled.div`
@@ -33,30 +33,57 @@ const AvatarInput = styled.input`
 const Name = styled.span`
   font-size: 22px;
 `
+const DefaultAvatarBtn = styled.label`
+  width: 30%;
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`
 export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
-  const onAvatarChange = async(e:React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e)
+
+  const onAvatarChange = async (e:React.ChangeEvent<HTMLInputElement>) => {
+
     const {files} = e.target
+
     if(user && files && files.length === 1) {
       const file = files[0]
       const locationRef = ref(storage, `avatars/${user?.uid}`)
       const result = await uploadBytes(locationRef, file)
       const photoURL = await getDownloadURL(result.ref)
+
       setAvatar(photoURL)
       await updateProfile(user, {photoURL})
     }
   }
 
+  const onDefaultAvatar = async () => {
+    if(user) {
+      setAvatar(null)
+      const photoRef = ref(storage,  `avatars/${user?.uid}`)
+      await deleteObject(photoRef)
+      await updateProfile(user, {photoURL:""})
+    }
+  }
+
   return <Wrapper>
     <AvatarUpload htmlFor="profile">
-      {Boolean(avatar) ? <AvatarImg src={avatar}/> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+      {Boolean(avatar)? <AvatarImg src={avatar}/> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-</svg>
-}
+</svg>}
     </AvatarUpload>
     <AvatarInput onChange={onAvatarChange} id="profile" type="file" accept="image/*"/>
+    <DefaultAvatarBtn onClick={onDefaultAvatar}>Change Default Avartar</DefaultAvatarBtn>
     <Name>
       {user?.displayName ?? "Anonymous"}
     </Name>
