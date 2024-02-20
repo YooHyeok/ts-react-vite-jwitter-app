e60805363ecd604e654e602fefe6ce6943e3ccd6
webdevyoo <yjk7454@naver.com>
2024-02-20 13:57:34 +0900

    feat: 로그인/회원가입 컴포넌트 useState 통합처리

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -10,54 +10,45 @@ export default function CreateAccount() {
   const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+
+  const [userInfo, setUserInfo] = useState({name: '', email: '', password: ''})
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {target: {name, value}} = e;
-    if(name === "name") {
-      setName(value);
-    } else if(name === "email") {
-      setEmail(value)
-    } else if(name === "password") {
-      setPassword(value)
-    }
+    // setUserInfo({...userInfo, [name]: value})
+    setUserInfo(currentUserInfo => ({...currentUserInfo, [name]: value})) // 콜백 상태업데이트
   }
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("")
-    if(isLoading || name === "" || email === "" || password === "") return;
+    if(isLoading || userInfo.name === "" || userInfo.email === "" || userInfo.password === "") return;
     try {
       setLoading(true)
       // 계정 생성
-      const credentials = await createUserWithEmailAndPassword(auth, email, password) //인증객체, 이메일, 패스워드 FireBase에 전달 -> 인증성공시 자격증명 반환
-      console.log(credentials.user)
+      const credentials = await createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password) //인증객체, 이메일, 패스워드 FireBase에 전달 -> 인증성공시 자격증명 반환
       // 사용자 프로필이름 지정
-      await updateProfile(credentials.user, {displayName: name,})
+      await updateProfile(credentials.user, {displayName: userInfo.name,})
       // 회원가입 성공시 홈페이지 리다이렉트
       navigate("/")
     } catch (e) {
       // setError
       if(e instanceof FirebaseError) { // 회원가입 실패시
-        console.log(e.code, e.message)
         setError(e.message)
       }
     } finally { // navigate에 의해 리다이렉트 되었더라도 무조건 실행된다.
       setLoading(false)
     }
-    console.log(name, email, password)
   }
 
   return (
   <Wrapper>
     <Title>Join ❌</Title>
     <Form onSubmit={onSubmit}>
-      <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required/>
-      <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required/>
-      <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required/>
+      <Input onChange={onChange} name="name" value={userInfo.name} placeholder="Name" type="text" required/>
+      <Input onChange={onChange} name="email" value={userInfo.email} placeholder="Email" type="email" required/>
+      <Input onChange={onChange} name="password" value={userInfo.password} placeholder="Password" type="password" required/>
       <Input onChange={onChange} type="submit" value={isLoading ? "Loading...." : "Create Account"}/>
     </Form>
     {error !== "" ? <Error>{error}</Error>: null}
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -9,28 +9,23 @@ import GithubButton from "../components/github-btn"
 export default function Login() {
   const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [userInfo, setUserInfo] = useState({email: '', password: ''})
   const [error, setError] = useState("")
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {target: {name, value}} = e;
-    if(name === "email") {
-      setEmail(value)
-    } else if(name === "password") {
-      setPassword(value)
-    }
+    setUserInfo(currentUserInfo => ({...currentUserInfo, [name]: value})) // 콜백 상태업데이트
+
   }
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("")
-    if(isLoading || email === "" || password === "") return;
+    if(isLoading || userInfo.email === "" || userInfo.password === "") return;
     try {
       setLoading(true)
       // 로그인
-      await signInWithEmailAndPassword(auth, email, password)
+      await signInWithEmailAndPassword(auth, userInfo.email, userInfo.password)
       // 로그인 성공시 홈페이지 리다이렉트
       navigate("/")
     } catch (e) {
@@ -41,15 +36,14 @@ export default function Login() {
     } finally { // navigate에 의해 리다이렉트 되었더라도 무조건 실행된다.
       setLoading(false)
     }
-    console.log(email, password)
   }
 
   return (
   <Wrapper>
     <Title>Log into ❌</Title>
     <Form onSubmit={onSubmit}>
-      <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required/>
-      <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required/>
+      <Input onChange={onChange} name="email" value={userInfo.email} placeholder="Email" type="email" required/>
+      <Input onChange={onChange} name="password" value={userInfo.password} placeholder="Password" type="password" required/>
       <Input onChange={onChange} type="submit" value={isLoading ? "Loading...." : "Log in"}/>
     </Form>
     {error !== "" ? <Error>{error}</Error>: null}
