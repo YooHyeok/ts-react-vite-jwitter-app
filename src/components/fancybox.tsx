438561ad3b5b7a09212aad87bcbe5f571d407e35
YooHyeok <yjk7454@naver.com>
2024-02-22 14:10:13 +0900

    feat: fancybox - 이미지 클릭시 확대 컴포넌트 추가

---
+++ src/components/fancybox.tsx
@@ -0,0 +1,49 @@
+import { useRef, useEffect } from "react";
+
+import { Fancybox as NativeFancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+export interface Avatar {
+  /* options : {
+    Carousel: {
+      infinite: boolean
+    }
+  } */
+  options? : object
+  delegate?: string
+  children?: React.ReactNode 
+}
+
+/**
+ * 이미지 확대 컴포넌트
+ * @description npm install --save \@fancyapps/ui
+ * @see https://fancyapps.com/
+ * @param props avatar URL
+ * @returns 
+ */
+function Fancybox(props: Avatar) {
+  const containerRef = useRef(null);
+  console.log(props)
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const delegate = props.delegate || "[data-fancybox]";
+    const options = props.options || {};
+
+    NativeFancybox.bind(container, delegate, options);
+
+    return () => {
+      NativeFancybox.unbind(container);
+      NativeFancybox.close();
+    };
+  });
+
+  return <div 
+            style={{ width:"115px", height:"115px"}}
+            ref={containerRef}
+          >
+            {props.children}
+          </div>;
+}
+
+export default Fancybox;
